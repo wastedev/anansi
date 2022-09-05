4511364febebb787076812b8bf6b626f38440a7d
Fima <kolyshkin.efim@mail.ru>
2022-09-05 17:30:49 +0300

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -57,6 +57,58 @@ const StyledFeedbackButtons = styled.div`
   }
 `
 
+//#region contactForm
+
+const StyledModal = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  visibility: hidden;
+  z-index: -1;
+  opacity: 0;
+  transition: all 0.09s ease-out;
+`
+
+const StyledModalOverlay = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background-color: black;
+  opacity: 0.7;
+  z-index: 0;
+`
+const StyledModalWindow = styled.div`
+  position: relative;
+  z-index: 2;
+  width: 550px;
+  border-radius: 10px;
+  box-shadow: 0 10px 15px rgba(0, 0, 0, 0.4);
+  background-color: white;
+  padding: 30px;
+`
+
+const StyledCloseButton = styled.button`
+  position: absolute;
+  right: 10px;
+  top: 10px;
+  border: none;
+  background: none;
+  opacity: 0.7;
+
+  :hover {
+    opacity: 1;
+  }
+`
+
+//#endregion
+
 const NavBar = () => {
   return (
     <StyledContainer>
